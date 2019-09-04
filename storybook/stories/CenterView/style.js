bfee4aebd29d0d47b764cb4a0ada39516e6ea793
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-09-04 11:49:07 +0300

    comment out unused stules

--- storybook/stories/CenterView/style.js
+++ storybook/stories/CenterView/style.js
@@ -2,7 +2,8 @@ export default {
     main: {
       flex: 1,
       justifyContent: 'center',
+      // paddingTop: 20,
       // alignItems: 'center',
-      backgroundColor: '#F5FCFF',
+      // backgroundColor: '#F5FCFF',
     },
   };
