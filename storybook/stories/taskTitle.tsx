ab6de0b11300a2c08f98206d03f9b9bb70d3179a
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-09-04 14:18:43 +0300

    add TaskTitle story

--- storybook/stories/taskTitle.tsx
+++ storybook/stories/taskTitle.tsx
@@ -1,14 +1,29 @@
 import { storiesOf } from '@storybook/react-native'
 import React from 'react'
 import { View as RView } from 'react-native'
-import TaskTitle from '../../components/TaskTitle'
+import { TaskStatus, TaskTitle } from '../../components/TaskTitle'
 
 const stories = storiesOf('Task Title header', module)
 stories.addDecorator(getStory => <RView style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>{getStory()}</RView>)
 
-stories.add('Header done', () => (
+stories.add('Header Done', () => (
     <TaskTitle {...{
-      title: 'My  title title title title title title title title title v',
+        status: TaskStatus.done,
+        title: 'My  title title title title title title title title title v',
+    }}
+    />
+  ))
+  .add('Header Active', () => (
+    <TaskTitle {...{
+        status: TaskStatus.active,
+        title: 'My  title title title title title title title title title v',
+    }}
+    />
+  ))
+  .add('Header Closed', () => (
+    <TaskTitle {...{
+        status: TaskStatus.closed,
+        title: 'My  title title title title title title title title title v',
     }}
     />
   ))
