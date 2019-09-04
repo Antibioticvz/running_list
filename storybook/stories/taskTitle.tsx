35ce07211cbb7521ae34f84924745ef1bd2771ed
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-09-04 15:16:26 +0300

    add knobs for title

--- storybook/stories/taskTitle.tsx
+++ storybook/stories/taskTitle.tsx
@@ -1,29 +1,31 @@
+import { text, withKnobs } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react-native'
 import React from 'react'
 import { View as RView } from 'react-native'
 import { TaskStatus, TaskTitle } from '../../components/TaskTitle'
 
 const stories = storiesOf('Task Title header', module)
+stories.addDecorator(withKnobs)
 stories.addDecorator(getStory => <RView style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>{getStory()}</RView>)
 
 stories.add('Header Done', () => (
     <TaskTitle {...{
         status: TaskStatus.done,
-        title: 'My  title title title title title title title title title v',
+        title: text('Title', 'Done title'),
     }}
     />
   ))
   .add('Header Active', () => (
     <TaskTitle {...{
         status: TaskStatus.active,
-        title: 'My  title title title title title title title title title v',
+        title: text('Title', 'Active title'),
     }}
     />
   ))
   .add('Header Closed', () => (
     <TaskTitle {...{
         status: TaskStatus.closed,
-        title: 'My  title title title title title title title title title v',
+        title: text('Title', 'Closed title'),
     }}
     />
   ))
